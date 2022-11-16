20ba0f20caa153e6941abdf9cf86a39be873588d
ZahidUllah <93036981+ZahidGuru57@users.noreply.github.com>
2022-11-16 19:18:34 +0500

    https://github.com/ZahidGuru57/javascriptcode.git

---
+++ loop.js
@@ -0,0 +1,40 @@
+// Loops
+// for loop
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+//  for (let i = 0; i < 10; console.log(i)) {
+//     i++;
+//  }
+
+// while loop
+// let i = 0;
+// while (i < 15) {
+//     console.log(i);
+//    i++;
+// }
+
+// let list = [2, 3, 4, 5, 6, 7, 8];
+// let i = 0;
+// while (i < list.length) {
+//     console.log(list[i]);
+//     i++;
+// }
+
+// do while loop
+// let i = 0;
+// do {
+//     console.log(i);
+//  i++;
+// }
+//  while (i < 10);
+
+
+//  let i = 0;
+// do {
+//     console.log(i);
+//  i++;
+// }
+//  while (i > 10);
