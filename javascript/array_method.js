57deb90781ec9a89f38e2938b84f12c6bc894dde
ZahidUllah <93036981+ZahidGuru57@users.noreply.github.com>
2022-11-17 16:31:32 +0500

    add string method file

--- javascript/array_method.js
+++ javascript/array_method.js
@@ -1,6 +1,28 @@
 // Array destructuring
-const names = ["zahid", "laiq", "jalil", "khan"];
+// const names = ["zahid", "laiq", "jalil", "khan"];
 
-const [name1, , name2] = names;
+// const [name1, , name2] = names;
 
-console.log(name2);
+// console.log(name2);
+
+const student_list = {
+    name: "zahid",
+    age: 32,
+    grade: "A",
+    marks: 44,
+    education: ["math", "algebra", "mathematics", "science"],
+    address: {
+        city: "Bangalore",
+        street: "Park Lane",
+        building: "12",
+    }
+    
+}
+
+const {name, address} = student_list;
+
+console.log(name,address);
+
+
+
+
---
+++ javascript/string_method.js
@@ -0,0 +1,18 @@
+// string concatenated method
+
+// const str1 = "Hello";
+// const str2 = "World";
+
+// const str3 = str1 + " " + str2;
+// console.log(str3);
+
+// const str4 = str1.concat(" " +str2);
+// console.log(str4);
+
+// const str5 = `${str1} ${str2}`;
+// console.log(str5);
+
+// indexOf method in string
+const str = "this is a new method";
+
+console.log(str.indexOf("new"));
